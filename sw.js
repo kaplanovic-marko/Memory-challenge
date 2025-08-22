f0aec5f7025c554b6b435cabf40ad38e2bf918c7
kaplanovic-marko <kaplanovic.marko@gmail.com>
2025-08-22 19:36:13 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+// Minimal offline cache for PWA
+const CACHE = 'memory-challenge-v1';
+const ASSETS = ['.', 'index.html', 'manifest.json', 'logo.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).catch(()=>caches.match('index.html')))
+  );
+});
